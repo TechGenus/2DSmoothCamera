dcfac577a0c09e11550cd7c0fd66221b466b841f
Janak <Janakkumar.Bhanushal@ryerson.ca>
2017-07-21 18:31:24 -0400

    First commit

---
+++ Assets/Scripts/CameraSmoothFollow.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSmoothFollow : MonoBehaviour {
+
+    public GameObject target;
+	public GameObject background;
+
+    public GameObject[] wall = new GameObject[2];
+    
+	public float speed;
+
+    private Vector3 camStartingPos;
+    private Vector3 targetPos;
+    private Vector3 leftWallPos;
+    private Vector3 rightWallPos;
+	private Vector3 bgPos;
+
+    private Camera cam;
+    
+	private float camHeight;
+    private float camWidth;
+	private float originalOffset;
+	private float camLeftEndPoint;
+	private float camRightEndPoint;
+	private float endToEndDistance;
+
+	// Use this for initialization
+	void Start () {
+        cam = GetComponent<Camera>();
+        
+		camStartingPos = transform.position;
+        leftWallPos = wall[0].GetComponent<Transform>().position;
+        rightWallPos = wall[1].GetComponent<Transform>().position;
+		bgPos = background.GetComponent<Transform> ().position;
+
+		camHeight = 2f * cam.orthographicSize;
+		camWidth = camHeight * cam.aspect;
+
+		camLeftEndPoint = leftWallPos.x + camWidth / 2f;
+		camRightEndPoint = rightWallPos.x - camWidth / 2f;
+		endToEndDistance = camRightEndPoint - camLeftEndPoint;
+
+		originalOffset = bgPos.x - transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		targetPos = target.GetComponent<Transform>().position;
+
+		camHeight = 2f * cam.orthographicSize; // these 2 lines are here because the user might change height and width from unity GUI
+		camWidth = camHeight * cam.aspect;
+
+		// Method to make camera follow target
+		cameraFollowTarget ();
+		// Method to move the bg
+		moveBg();
+	}
+
+	void moveBg() {
+		float camPosPercentage = (transform.position.x / endToEndDistance) * 2f;
+
+		float newOffset = (originalOffset - originalOffset * camPosPercentage);
+		Vector3 newPos = new Vector3 (transform.position.x + newOffset, bgPos.y, bgPos.z);
+
+		background.transform.position = newPos;
+	}
+
+	void cameraFollowTarget() {
+		if (!(targetPos.x >= camLeftEndPoint))
+		{
+			translateCamHorizontallyTo(camLeftEndPoint);
+		}
+		else if (!(targetPos.x <= camRightEndPoint))
+		{
+			translateCamHorizontallyTo(camRightEndPoint);
+		}
+		else
+		{
+			translateCamHorizontallyTo(targetPos.x);
+		}
+	}
+
+    void translateCamHorizontallyTo(float x)
+    {
+        Vector3 newPos = new Vector3(x, transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPos, speed);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float[] speed = new float[2];
+
+    private Rigidbody rb;
+
+	private float hAxis;
+	private float jumpAxis;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lockZAxis();
+
+        hAxis = Input.GetAxis("Horizontal"); // this takes keyboard input from left/right arrows as well as the a and d keys
+        jumpAxis = Input.GetAxis("Jump"); // this takes keyboard input from the space bar
+
+        Vector3 offset = new Vector3(hAxis * speed[0], 0, 0);
+        // rb.MovePosition(transform.position + offset); // this moves rigidbody so it is less smooth
+		transform.position += offset * Time.deltaTime; // moves character to new position that is old position + offset
+
+        if (isGrounded() && jumpAxis > 0)
+            jump();
+	}
+
+    bool isGrounded()
+    {
+        // casting a ray downwards from the center of the character, if the ray hits something it means the character is grounded
+        return Physics.Raycast(transform.position, Vector3.down, transform.localScale.y / 1.5f);
+    }
+
+    void jump()
+    {
+		rb.velocity += new Vector3 (0, jumpAxis * speed [1], 0) * Time.deltaTime * 10; // adds jump speed to current y axis velocity of object
+    }
+
+    void lockZAxis()
+    {
+        Vector3 pos = transform.position;
+        pos.z = 0;
+        transform.position = pos;
+    }
+}
